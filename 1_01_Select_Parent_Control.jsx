b16f8b7add54638d048dd3882bdcfaf841bcf91e
NickNickWaterton <98813145+NickNickWaterton@users.noreply.github.com>
2022-02-01 10:50:12 +0000

    Update 1_01_Select_Parent_Control.jsx

--- 1_01_Select_Parent_Control.jsx
+++ 1_01_Select_Parent_Control.jsx
@@ -1,4 +1,10 @@
-﻿var rigLibNotIncluded = typeof rigLib === "undefined";
+﻿﻿//~ This file is part of Ae Rig Anim.
+//~ Ae Rig Anim is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.
+//~ Ae Rig Anim is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License version 3 for more details.
+//~ You should have received a copy of the GNU General Public License version 3 along with Ae Rig Anim. If not, see <https://www.gnu.org/licenses/>.
+//~ Copyright Nicholas Waterton and Arcus Animation Studios
+
+var rigLibNotIncluded = typeof rigLib === "undefined";
 if ( rigLibNotIncluded ) {
     #include 1_00_Rig_Scripts_Lib.jsx
 }
